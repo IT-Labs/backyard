94a6837d33dd678444f601acde23ae1442e381c0
jovica.krstevski <jovica.krstevski@it-labs.com>
2020-05-31 09:39:57 +0200

    adding build info and handle error page

--- api/src/main/java/com/itlabs/api/configuration/SwaggerConfig.java
+++ api/src/main/java/com/itlabs/api/configuration/SwaggerConfig.java
@@ -1,9 +1,14 @@
 package com.itlabs.api.configuration;
 
+import java.util.Collections;
+import java.util.Date;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,12 +16,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+  private final BuildProperties buildProperties;
+
+  public SwaggerConfig(BuildProperties buildProperties){
+    this.buildProperties = buildProperties;
+  }
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any())
-        .build();
+        .build().apiInfo(apiInfo());
+  }
+  private ApiInfo apiInfo() {
+    return new ApiInfo(
+            buildProperties.getName(),
+            String.format(" Application Build at %s and Started at %s", buildProperties.getTime(), new Date().toInstant()),
+            buildProperties.getVersion(),
+            "",
+            new Contact("Jovica Krstevski", "http://it-labs.com", "jovica.krstevski@it-labs.com"),
+            "MIT", "https://github.com/IT-Labs/backyard/blob/master/LICENSE", Collections.emptyList());
   }
 }
