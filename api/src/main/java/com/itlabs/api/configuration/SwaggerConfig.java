41157ad675b13be5a84622ac68d1f8033d6c17bd
jovica.krstevski <jovica.krstevski@it-labs.com>
2020-05-31 10:22:30 +0200

    reorganize swagger pages

--- api/src/main/java/com/itlabs/api/configuration/SwaggerConfig.java
+++ api/src/main/java/com/itlabs/api/configuration/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.itlabs.api.configuration;
 
+import com.google.common.base.Predicates;
 import java.util.Collections;
 import java.util.Date;
 import org.springframework.boot.info.BuildProperties;
@@ -9,33 +10,56 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
   private final BuildProperties buildProperties;
+  static final String ACTUATOR = ".*/actuator.*";
 
-  public SwaggerConfig(BuildProperties buildProperties){
+  public SwaggerConfig(BuildProperties buildProperties) {
     this.buildProperties = buildProperties;
   }
+
   @Bean
   public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
+    return new Docket(SWAGGER_2)
+        .groupName("api")
         .select()
         .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any())
-        .build().apiInfo(apiInfo());
+        .paths(Predicates.not(PathSelectors.regex("/error.*")))
+        .paths(Predicates.not(PathSelectors.regex(ACTUATOR)))
+        .build()
+        .apiInfo(apiInfo(buildProperties.getName()));
   }
-  private ApiInfo apiInfo() {
+
+  @Bean
+  public Docket actuatorApi() {
+    return new Docket(SWAGGER_2)
+        .groupName("monitoring-api")
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(regex(ACTUATOR))
+        .build()
+        .apiInfo(apiInfo("Actuator"));
+  }
+
+  private ApiInfo apiInfo(String name) {
+
     return new ApiInfo(
-            buildProperties.getName(),
-            String.format(" Application Build at %s and Started at %s", buildProperties.getTime(), new Date().toInstant()),
-            buildProperties.getVersion(),
-            "",
-            new Contact("Jovica Krstevski", "http://it-labs.com", "jovica.krstevski@it-labs.com"),
-            "MIT", "https://github.com/IT-Labs/backyard/blob/master/LICENSE", Collections.emptyList());
+            name,
+        String.format(
+            " Application Build at %s and Started at %s",
+            buildProperties.getTime(), new Date().toInstant()),
+        buildProperties.getVersion(),
+        "",
+        new Contact("Jovica Krstevski", "http://it-labs.com", "jovica.krstevski@it-labs.com"),
+        "MIT",
+        "https://github.com/IT-Labs/backyard/blob/master/LICENSE",
+        Collections.emptyList());
   }
 }
--- api/src/main/java/com/itlabs/api/controllers/RootController.java
+++ api/src/main/java/com/itlabs/api/controllers/RootController.java
@@ -3,8 +3,10 @@ package com.itlabs.api.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
+@ApiIgnore
 public class RootController {
     @GetMapping(value = "/")
     public ResponseEntity<String> get() {
