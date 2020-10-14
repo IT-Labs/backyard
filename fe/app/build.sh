7561099110d3e9cf3edb81f55b39582ce403e64c
jovica.krstevski <jovica.krstevski@it-labs.com>
2020-10-14 09:11:40 +0200

    fix env sending to react  docker

--- fe/app/build.sh
+++ fe/app/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 docker-compose down
 docker build --tag fe-sample .
-docker-compose  up -d
--- fe/app/src/service/config.ts
+++ fe/app/src/service/config.ts
@@ -5,12 +5,12 @@ const apiVersion = "api/v1/";
  
   var appConfig: IAppConfig = Object.assign({});
   
-  if(process.env.API_URL){
-    appConfig.urlApi= process.env.API_URL +apiVersion;
+  if(process.env.REACT_APP_API_URL){
+    appConfig.urlApi= process.env.REACT_APP_API_URL +apiVersion;
 }  else{  
 appConfig.urlApi= host +apiVersion;
 }
 console.info("env", process.env);
-console.info("env.API_URL", process.env.API_URL);
+console.info("env.REACT_APP_API_URL", process.env.REACT_APP_API_URL);
 console.info("app config", appConfig);
   export { appConfig };
