596bdf20df1e6a2886b49876e1d59afa5a0f0548
jovica.krstevski <jovica.krstevski@it-labs.com>
2020-11-14 10:02:36 +0100

    fix clean script

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 docker-compose -f "api/docker-compose.yml" down
 docker-compose -f "api-gateway/docker-compose.yml" down
-docker-compose -f "fe/e2e_tests/docker-compose-grid.yml" down
-docker-compose -f "docker-compose.yml" down
+docker-compose -f "docker-compose-grid.yml" down
+docker-compose -f "fe/e2e_tests/docker-compose.yml" down
 docker-compose -f "fe/app/docker-compose.yml" down
 docker-compose -f "jmeter/docker-compose.yml"  down
 docker-compose -f "docker-compose-sonar.yml"  down
