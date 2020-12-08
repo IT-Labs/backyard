4e56448491f7c7a07f57548d7d513d07c3c13ca1
jovica.krstevski <jovica.krstevski@it-labs.com>
2020-12-08 11:58:58 +0100

    setup git hook

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ docker-compose   -f "api/docker-compose.yml" up -d
 docker-compose   -f "api-gateway/docker-compose.yml" up -d
 docker-compose   -f "docker-compose-grid.yml" up -d
 docker-compose   -f "fe/app/docker-compose.yml" up -d
+./setup-git-hook.sh
