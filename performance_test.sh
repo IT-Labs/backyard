8b9a4e12d2b74d6f55dbe4c5776a8d6915b5a78a
jovica.krstevski <jovica.krstevski@it-labs.com>
2021-01-04 08:48:31 +0100

    refactor performance_test

--- performance_test.sh
+++ performance_test.sh
@@ -2,5 +2,12 @@
 docker-compose -f  "docker-compose-monitoring.yml" up -d
 root=$1
 echo 'sitespeed volume folder-> '$root
-#docker run --network=sample-network --rm -v $root:/sitespeed.io sitespeedio/sitespeed.io:16.0.1-plus1 --graphite.host=graphite-sample publicUrls.txt -b chrome -n 2
-docker run --network=sample-network --rm -v $root:/sitespeed.io sitespeedio/sitespeed.io:16.0.1 --graphite.host=graphite-sample  --browsertime.my.host="http://fe-dev:3000/"  --browsertime.my.password="sample" --browsertime.my.username="sample" --browsertime.my.pages='items,Items Page;about,About Page'  privateUrls.js --multi -b chrome -n 1
+echo 'private urls run'
+docker run --network=sample-network --rm -v $root:/sitespeed.io sitespeedio/sitespeed.io:16.0.1 \
+ --graphite.host=graphite-sample  --browsertime.my.host="http://fe-dev:3000/"  --browsertime.my.password="sample" \
+ --browsertime.my.username="sample" \
+ --browsertime.my.pages='items,Items Page;about,About Page' \
+privateUrls.js --multi -b chrome -n 1
+echo 'public urls run'
+ docker run --network=sample-network --rm -v $root:/sitespeed.io sitespeedio/sitespeed.io:16.0.1-plus1 \
+ --graphite.host=graphite-sample publicUrls.txt -b chrome -n 1
