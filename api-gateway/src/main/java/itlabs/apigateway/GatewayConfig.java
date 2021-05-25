f6889115f543d5a5609b9b054d793843cc1a7161
jovica.krstevski <jovica.krstevski@it-labs.com>
2021-05-25 07:43:53 +0200

    API Gateway upgrade to Java 15 and other libraries

--- api-gateway/src/main/java/itlabs/apigateway/GatewayConfig.java
+++ api-gateway/src/main/java/itlabs/apigateway/GatewayConfig.java
@@ -4,8 +4,10 @@ import java.util.function.Function;
 import org.springframework.cloud.gateway.filter.factory.DedupeResponseHeaderGatewayFilterFactory.Strategy;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.Route.AsyncBuilder;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
 import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -33,30 +35,37 @@ public class GatewayConfig {
 				.route(getPublicRoute(internalServiceConfiguration)).build();
 	}
 
-	private Function<PredicateSpec, AsyncBuilder> getItemsRoute(
+	private Function<PredicateSpec, Buildable<Route>> getItemsRoute(
 			InternalServiceConfiguration internalServiceConfiguration) {
 		log.info(" internal api uri {}", internalServiceConfiguration.getInternalApiUrl());
-		final Function<PredicateSpec, AsyncBuilder> predicateSpecAsyncBuilderFunction = r -> r.path("/items/**")
+		final Function<PredicateSpec, Buildable<Route>> predicateSpecAsyncBuilderFunction = r -> r.path("/items/**")
 				.filters(f -> f.rewritePath("/items", "/api/v1/items").removeRequestHeader("Cookie")
 						.dedupeResponseHeader("Access-Control-Allow-Headers", Strategy.RETAIN_UNIQUE.toString())
 						.dedupeResponseHeader("Access-Control-Allow-Origin", Strategy.RETAIN_UNIQUE.toString())
-						.requestRateLimiter(c -> c.setRateLimiter(getItemsRateLimiter())).retry(5)
-						.hystrix(x -> x.setName("FallbackInternal").setFallbackUri("forward:/fallback/message")))
-				.uri(internalServiceConfiguration.getInternalApiUrl()).id("PublicModule");
+						.requestRateLimiter(c -> c.setRateLimiter(getItemsRateLimiter())).retry(5).fallbackHeaders(
+								x -> x.hy.setName("FallbackInternal").setFallbackUri("forward:/fallback/message"))
+				// .hystrix(x ->
+				// x.setName("FallbackInternal").setFallbackUri("forward:/fallback/message"))
+				).uri(internalServiceConfiguration.getInternalApiUrl())// .id("PublicModule")
+		;
 		// forward is not performed due cors issues
 		return predicateSpecAsyncBuilderFunction;
 	}
 
-	private Function<PredicateSpec, AsyncBuilder> getPublicRoute(
+	private Function<PredicateSpec, Buildable<Route>> getPublicRoute(
 			InternalServiceConfiguration internalServiceConfiguration) {
-		final Function<PredicateSpec, AsyncBuilder> predicateSpecAsyncBuilderFunction = r -> r.path("/home/**")
-				.filters(f -> f.rewritePath("/home", "/public/home").removeRequestHeader("Cookie")
-						.dedupeResponseHeader("Access-Control-Allow-Headers", Strategy.RETAIN_UNIQUE.toString())
-						.dedupeResponseHeader("Access-Control-Allow-Origin", Strategy.RETAIN_UNIQUE.toString())
-						.requestRateLimiter(
-								c -> c.setRateLimiter(getItemsRateLimiter()).setKeyResolver(ipKeyResolver()))
-						.retry(3).hystrix(x -> x.setName("FallbackPublic").setFallbackUri("forward:/fallback/message")))
-				.uri(internalServiceConfiguration.getInternalApiUrl()).id("itemsModule");
+		final Function<PredicateSpec, Buildable<Route>> predicateSpecAsyncBuilderFunction = r -> r.path("/home/**")
+				.filters(
+						f -> f.rewritePath("/home", "/public/home").removeRequestHeader("Cookie")
+								.dedupeResponseHeader("Access-Control-Allow-Headers", Strategy.RETAIN_UNIQUE.toString())
+								.dedupeResponseHeader("Access-Control-Allow-Origin", Strategy.RETAIN_UNIQUE.toString())
+								.requestRateLimiter(
+										c -> c.setRateLimiter(getItemsRateLimiter()).setKeyResolver(ipKeyResolver()))
+								.retry(3)
+				// .hystrix(x ->
+				// x.setName("FallbackPublic").setFallbackUri("forward:/fallback/message"))
+				).uri(internalServiceConfiguration.getInternalApiUrl())// .id("itemsModule")
+		;
 		return predicateSpecAsyncBuilderFunction;
 	}
 
