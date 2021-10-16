18f707b99c282d03930ad201a30aa9f9dc06d2bc
jovica.krstevski <jovica.krstevski@it-labs.com>
2021-10-16 09:42:35 +0200

    sitespeed upgrade

--- performance_test.sh
+++ performance_test.sh
@@ -3,11 +3,11 @@ docker-compose -f  "docker-compose-monitoring.yml" up -d
 root=$1
 echo 'sitespeed volume folder-> '$root
 echo 'private urls run'
-docker run --network=sample-network --rm -v $root:/sitespeed.io sitespeedio/sitespeed.io:16.0.1 \
+docker run --network=sample-network --rm -v $root:/sitespeed.io sitespeedio/sitespeed.io:20.2.0 \
  --graphite.host=graphite-sample  --browsertime.my.host="http://fe-dev:3000/"  --browsertime.my.password="sample" \
  --browsertime.my.username="sample" \
  --browsertime.my.pages='items,Items Page;about,About Page' \
 privateUrls.js --multi -b chrome -n 1
 echo 'public urls run'
- docker run --network=sample-network --rm -v $root:/sitespeed.io sitespeedio/sitespeed.io:16.0.1-plus1 \
- --graphite.host=graphite-sample publicUrls.txt -b chrome -n 1
+ docker run --network=sample-network --rm -v $root:/sitespeed.io sitespeedio/sitespeed.io:20.2.0-plus1 \
+ --graphite.host=graphite-sample publicUrls.txt -b edge -n 1
