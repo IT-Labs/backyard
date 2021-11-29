6572eff6edf6ac6c28bcf83212f6b59114a90b46
jovica.krstevski <jovica.krstevski@it-labs.com>
2021-11-29 08:17:16 +0100

    jdbc config option

---
+++ api-config/src/main/resources/db/migration/V1.1__Properties.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS properties
+(
+  id serial primary key,
+  key character varying(255) NOT NULL,
+  value text NOT NULL,
+  application character varying(100) NOT NULL DEFAULT 'application',
+  label character varying(100) NOT NULL DEFAULT 'label',
+  profile character varying(100)  NOT NULL DEFAULT 'default',
+  updated timestamp without time zone NOT NULL DEFAULT now(),
+  created timestamp without time zone NOT NULL DEFAULT now()
+
+);
+ALTER TABLE properties DROP CONSTRAINT IF EXISTS UC_properties_application_label_profile_key;
+ALTER TABLE properties ADD CONSTRAINT UC_properties_application_label_profile_key UNIQUE (key, application, profile, label);
