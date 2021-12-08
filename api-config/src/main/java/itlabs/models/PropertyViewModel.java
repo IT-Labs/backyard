5dc109a21c7f6a9a99275a2b41a2c109563853b3
jovica.krstevski <jovica.krstevski@it-labs.com>
2021-12-08 08:31:13 +0100

    fix warning logs

--- api-config/src/main/java/itlabs/models/PropertyViewModel.java
+++ api-config/src/main/java/itlabs/models/PropertyViewModel.java
@@ -2,8 +2,10 @@ package itlabs.models;
 
 import java.util.UUID;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class PropertyViewModel extends  PropertyModel{
    UUID uuid ;
 }
--- api-config/src/main/java/itlabs/repositories/Property.java
+++ api-config/src/main/java/itlabs/repositories/Property.java
@@ -3,10 +3,12 @@ package itlabs.repositories;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Data
 @Table(name = "properties")
+@EqualsAndHashCode(callSuper=false)
 public class  Property extends  BaseEntity {
 
     String value;
