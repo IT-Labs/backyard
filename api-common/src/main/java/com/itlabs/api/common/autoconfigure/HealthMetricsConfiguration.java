30c2ff78bff6132e55b8d8c3f19539caf397c79d
jovica.krstevski <jovica.krstevski@it-labs.com>
2022-03-28 11:15:21 +0200

    set target java version to 11

--- api-common/src/main/java/com/itlabs/api/common/autoconfigure/HealthMetricsConfiguration.java
+++ api-common/src/main/java/com/itlabs/api/common/autoconfigure/HealthMetricsConfiguration.java
@@ -6,9 +6,12 @@ import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.CompositeHealthContributor;
+import org.springframework.boot.actuate.health.CompositeReactiveHealthContributor;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthContributor;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.ReactiveHealthContributor;
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
@@ -17,62 +20,77 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @ConditionalOnClass({MeterRegistry.class, Gauge.class, Health.class, HealthIndicator.class})
 public class HealthMetricsConfiguration {
-
   private static final int UNKNOWN = 0;
-
   private static final int SERVICE_DOWN = 1;
-
   private static final int OUT_OF_SERVICE = 2;
-
   private static final int SERVICE_UP = 3;
-
-  public static final String ACTUATOR_HEALTH_METRIC_NAME = "actuator_health_sample";
-
+  public static final String ACTUATOR_HEALTH_METRIC_NAME = "actuator_health_sample-1.0.0";
   public static final String ACTUATOR_HEALTH_TAG_SERVICE_NAME = "service";
-
   public static final String ACTUATOR_HEALTH_TAG_INDICATOR_NAME = "indicator";
-
   private final MeterRegistry registry;
-
   private final String applicationName;
 
   public HealthMetricsConfiguration(
       MeterRegistry registry,
       Map<String, HealthContributor> healthContributors,
+      Map<String, ReactiveHealthContributor> reactiveHealthContributors,
       @Value("${spring.application.name}") String applicationName) {
     this.registry = registry;
     this.applicationName = applicationName;
     setMeterStatusFromHealthContributors(healthContributors);
+    setMeterStatusFromReactiveHealthContributors(reactiveHealthContributors);
   }
 
   private void setMeterStatusFromHealthContributors(
-      Map<String, HealthContributor> healthContributors) {
+      final Map<String, HealthContributor> healthContributors) {
     healthContributors.forEach(
         (name, contributor) -> {
           if (contributor instanceof HealthIndicator) {
-
             setHealthStatusInMeterRegistry(name, (HealthIndicator) contributor);
           } else if (contributor instanceof CompositeHealthContributor) {
-            CompositeHealthContributor healthContributor = (CompositeHealthContributor) contributor;
+            var healthContributor = (CompositeHealthContributor) contributor;
             healthContributor.forEach(
                 x ->
                     setHealthStatusInMeterRegistry(
                         x.getName(), (HealthIndicator) x.getContributor()));
           } else {
-            throwIllegalStateException(name);
+            throw new IllegalStateException(
+                String.format("Unknown HealthContributor type: %s", name));
           }
         });
   }
 
-  private void throwIllegalStateException(String name) {
-    throw new IllegalStateException(String.format("Unknown HealthContributor type: %s", name));
+  private void setMeterStatusFromReactiveHealthContributors(
+      final Map<String, ReactiveHealthContributor> reactiveHealthContributors) {
+    reactiveHealthContributors.forEach(
+        (name, contributor) -> {
+          if (contributor instanceof ReactiveHealthIndicator) {
+
+            setHealthStatusInMeterRegistry(name, ((ReactiveHealthIndicator) contributor));
+          } else if (contributor instanceof CompositeReactiveHealthContributor) {
+            var healthContributor = (CompositeReactiveHealthContributor) contributor;
+            healthContributor.forEach(
+                x ->
+                    setHealthStatusInMeterRegistry(
+                        x.getName(), ((ReactiveHealthIndicator) x.getContributor())));
+          } else {
+            throw new IllegalStateException(
+                String.format("Unknown HealthContributor type: %s", name));
+          }
+        });
   }
 
-  private void setHealthStatusInMeterRegistry(String name, HealthIndicator contributor) {
+  private void setHealthStatusInMeterRegistry(
+      final String name, final HealthIndicator contributor) {
     setHealthStatusInMeterRegistry(name, contributor.health());
   }
 
-  private void setHealthStatusInMeterRegistry(String name, Health health) {
+  private void setHealthStatusInMeterRegistry(
+      final String name, final ReactiveHealthIndicator contributor) {
+    setHealthStatusInMeterRegistry(name, contributor.health().block());
+  }
+
+  private void setHealthStatusInMeterRegistry(final String name, final Health health) {
     log.debug("setting {} service health status in meter registry ", name);
     Gauge.builder(ACTUATOR_HEALTH_METRIC_NAME, health, this::getStatusCode)
         .strongReference(true)
@@ -81,8 +99,8 @@ public class HealthMetricsConfiguration {
         .register(registry);
   }
 
-  private int getStatusCode(Health health) {
-    Status status = health.getStatus();
+  private int getStatusCode(final Health health) {
+    var status = health.getStatus();
     if (Status.UP.equals(status)) {
       return SERVICE_UP;
     }
